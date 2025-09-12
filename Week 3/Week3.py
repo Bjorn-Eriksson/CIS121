6e6ca77fa2077bd097ba67b7c71f3f446d0446a2
Bjorn-Eriksson <hf8805ua@go.minnstate.edu>
2025-09-12 15:43:01 -0500

    More work on Quiz Questions

--- Week 3/Week3.py
+++ Week 3/Week3.py
@@ -1,1 +1,58 @@
 #Created by Bjorn Eriksson 
+
+#Week 3 Quiz Questions
+
+#Question 1:
+
+
+
+
+
+
+#Question 5 
+'''
+num_1 = float(input("Enter number 1"))
+num_2 = float(input("Enter number 2"))
+num_3 = float(input("Enter number 3"))
+
+#Starting code
+if num_1 <= num_2 <= num_3:
+    print(num_1, num_2, num_3)
+elif num_1 <= num_3 <= num_2:
+    print(num_1, num_3, num_2)
+'''
+
+
+
+#Question 6, Just reversing the order of Quesiton 5
+
+
+
+
+
+#Question 7, using a lot of modulus (%)
+
+#29 knuts = 1 sickle
+#17 sickles = 1 galleon
+
+#1 galleon = 493 Knuts
+
+
+#Two ways to approach, top down or down up.
+
+knuts = int(input("Enter amount of knuts: "))
+
+#Find if we can buy any galleons
+galleons = knuts // (29 * 17)
+knuts_remaining = knuts % (29 * 17)
+
+#See if we can buy sickles
+sickles = knuts_remaining // 29
+knuts_final = knuts_remaining % 29
+
+
+
+
+
+
+
